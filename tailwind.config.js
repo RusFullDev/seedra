3939e7f2e2825d5fdf0f2d31aaf469495b903671
RusFullDev <urinrust@gmail.com>
2024-05-21 09:11:49 +0500

    new project seedra

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container:{
+center:true,
+padding:'20px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
